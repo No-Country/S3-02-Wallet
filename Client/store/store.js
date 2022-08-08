6982c90c7241158de9e434a29103417364f48488
beckman924 <braiancarabajal@gmail.com>
2022-08-07 23:12:32 -0300

    Re Setup Landing, Login, Register and Home pages

---
+++ Client/store/store.js
@@ -0,0 +1,60 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { createWrapper } from "next-redux-wrapper";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "./sync_storage";
+
+import users from "./usersSlice";
+import counter from "./countSlice";
+
+const { persistStore, persistReducer } = require("redux-persist");
+
+const combinedReducers = combineReducers({
+  counter,
+  users,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  whitelist: ["counter", "users"],
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
+
+const makeStore = ({ isServer }) => {
+  if (isServer) {
+    return configureStore({
+      reducer: persistedReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
+    });
+  } else {
+    const store = configureStore({
+      reducer: persistedReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          },
+        }),
+    });
+
+    store.__persistor = persistStore(store);
+
+    return store;
+  }
+};
+
+export const wrapper = createWrapper(makeStore);
